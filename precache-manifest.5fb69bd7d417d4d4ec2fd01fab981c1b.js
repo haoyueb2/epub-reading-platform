841d4f51d8b01dcb41010dfb4da1a623e26c6c40
mooooon333 <742932097@qq.com>
2019-06-09 16:38:55 +0800

    Updates

---
+++ precache-manifest.5fb69bd7d417d4d4ec2fd01fab981c1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd5b452f530851e829b4496c705e93a",
+    "url": "/epub-reading-platform/index.html"
+  },
+  {
+    "revision": "aa165bd6dbcb24612efe",
+    "url": "/epub-reading-platform/static/css/2.d987b720.chunk.css"
+  },
+  {
+    "revision": "cab3fa1c56fa4c650a2d",
+    "url": "/epub-reading-platform/static/css/main.ab886f8d.chunk.css"
+  },
+  {
+    "revision": "aa165bd6dbcb24612efe",
+    "url": "/epub-reading-platform/static/js/2.e8fa2cad.chunk.js"
+  },
+  {
+    "revision": "cab3fa1c56fa4c650a2d",
+    "url": "/epub-reading-platform/static/js/main.eb1152dc.chunk.js"
+  },
+  {
+    "revision": "bcb4d6eece43c3645168",
+    "url": "/epub-reading-platform/static/js/runtime~main.d765a1d9.js"
+  },
+  {
+    "revision": "e4a4f6ac8fd52436426cfbeb68d34a30",
+    "url": "/epub-reading-platform/static/media/banner.e4a4f6ac.jpg"
+  },
+  {
+    "revision": "6d2bda28638cbccf34e267a4252a20b6",
+    "url": "/epub-reading-platform/static/media/login.6d2bda28.jpg"
+  }
+]);
